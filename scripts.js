01d0852e8b11427b1c61a4918cc6a6cf8da9c3a1
VictorObioha <victor.obiohac@gmail.com>
2024-07-22 07:33:41 +0100

    a

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let healthQuestions = [
+"Do you have any chronic illness?",
+"Do you exercise regularly?",
+"Do you use tobacco products?",
+"Do you drink alcohol excessively?",
+"Do you have a healthy diet?",
+"Do you get enough sleep?",
+"Do you experience stress or anxiety?",
+"Have you had a recent check-up?",
+"Do you have allergies?",
+"Do you take any medications regularly?"
+];
---
+++ syntax.py
@@ -0,0 +1,24 @@
+print('english to spanish')
+n=int(input("enter a digit from 0-9:"))
+if n==0:
+    print("cero")
+elif n==1:
+    print("uno")
+elif n==2:
+    print("dos")
+elif n==3:
+    print("tres")
+elif n==4:
+    print("cuatro")
+elif n==5:
+    print("cinco")
+elif n==6:
+    print("seis")
+elif n==7:
+    print("siete")
+elif n==8:
+    print("ocho")
+elif n==9:
+    print("nueve")
+else:
+    print("por favor ingrese un digito del 0 al 9")
